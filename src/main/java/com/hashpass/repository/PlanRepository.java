4218b3a6ddc828a87a4974537b518172e57f66da
yagooo_cn <yago.contreras.nevares@gmail.com>
2026-02-20 11:23:13 +0100

    Repositories añadidos

---
+++ src/main/java/com/hashpass/repository/PlanRepository.java
@@ -0,0 +1,9 @@
+package com.hashpass.repository;
+
+import com.hashpass.model.Plan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlanRepository extends JpaRepository<Plan, Long> {
+}
