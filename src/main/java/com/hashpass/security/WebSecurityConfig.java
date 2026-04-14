203771bf35ab78c3f4329eaa564c38f8b9eac7ff
IML15 <g6nhwnwjy2@privaterelay.appleid.com>
2026-04-14 09:42:38 +0200

    Cambios en el SecurityConfig para implementar la API

--- src/main/java/com/hashpass/security/WebSecurityConfig.java
+++ src/main/java/com/hashpass/security/WebSecurityConfig.java
@@ -3,14 +3,19 @@ package com.hashpass.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
@@ -50,7 +55,8 @@ public class WebSecurityConfig {
 	}
 
 	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	@Order(2)
+	public SecurityFilterChain webFilterChain(HttpSecurity http) throws Exception {
 
 		http.authenticationProvider(authenticationProvider());
 
@@ -191,5 +197,50 @@ public class WebSecurityConfig {
 		} catch (Exception e) {
 			throw new RuntimeException("Error generando la llave maestra", e);
 		}
+	}
+
+	@Bean
+	@Order(1)
+	public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
+
+		http.authenticationProvider(authenticationProvider());
+
+		http
+				.securityMatcher("/api/**");
+				//.exceptionHandling(handling -> handling.authenticationEntryPoint(unauthorizedHandlerJwt));
+
+		http
+				.authorizeHttpRequests(authorize -> authorize
+						// PRIVATE ENDPOINTS
+						// Images
+						//.requestMatchers(HttpMethod.PUT, "/api/images/*/media").hasRole("USER")
+						//.requestMatchers(HttpMethod.DELETE, "/api/books/*/images/*").hasRole("USER")
+						// Books
+						//.requestMatchers(HttpMethod.POST, "/api/books/**").hasRole("USER")
+						//.requestMatchers(HttpMethod.PUT, "/api/books/**").hasRole("USER")
+						//.requestMatchers(HttpMethod.DELETE, "/api/books/**").hasRole("ADMIN")
+						// Shops
+						//.requestMatchers(HttpMethod.PUT, "/api/shops/**").hasRole("ADMIN")
+						//.requestMatchers(HttpMethod.PUT, "/api/shops/**").hasRole("ADMIN")
+						//.requestMatchers(HttpMethod.DELETE, "/api/shops/**").hasRole("ADMIN")
+						// PUBLIC ENDPOINTS
+						.anyRequest().permitAll());
+
+		// Disable Form login Authentication
+		http.formLogin(formLogin -> formLogin.disable());
+
+		// Disable CSRF protection (it is difficult to implement in REST APIs)
+		http.csrf(csrf -> csrf.disable());
+
+		// Disable Basic Authentication
+		http.httpBasic(httpBasic -> httpBasic.disable());
+
+		// Stateless session
+		http.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+		// Add JWT Token filter
+		//http.addFilterBefore(new JwtRequestFilter(userDetailService, jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+
+		return http.build();
 	}
 }
